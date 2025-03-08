26aa07443c28c0b9c876fc64e738dc0f25640db2
Jonathan Leeming <jonathan@leeming.dev>
2025-03-08 21:56:44 +0000

    added proper tests

--- embryon-lang/src/lib.rs
+++ embryon-lang/src/lib.rs
@@ -1,7 +1,7 @@
 use crate::compile::Compiler;
 use inkwell::context::Context;
-use inkwell::targets::{CodeModel, FileType, InitializationConfig, RelocMode, Target, TargetMachine, TargetTriple};
-use inkwell::OptimizationLevel;
+// use inkwell::targets::{CodeModel, FileType, InitializationConfig, RelocMode, Target, TargetMachine, TargetTriple};
+// use inkwell::OptimizationLevel;
 use std::path::Path;
 
 pub mod ast;
@@ -26,67 +26,200 @@ pub fn compile(program: ast::Module, path: &Path) {
     let mut compiler = Compiler::new(&context, &builder, &module);
     compiler.compile_module(&program).unwrap();
 
-    Target::initialize_arm(&InitializationConfig::default());
-    let target = Target::from_triple(&TargetTriple::create("thumbv7em-none-eabi")).unwrap();
-    let target_machine = target
-        .create_target_machine(
-            &TargetMachine::get_default_triple(),
-            "cortex-m4",
-            "",
-            OptimizationLevel::Default,
-            RelocMode::Default,
-            CodeModel::Default,
-        )
-        .unwrap();
+    // Write ll to file
+    module.print_to_file(path.with_extension("ll")).unwrap();
 
-    target_machine
-        .write_to_file(&module, FileType::Assembly, &path.with_extension("s"))
-        .unwrap();
+    // Target::initialize_arm(&InitializationConfig::default());
+    // let target = Target::from_triple(&TargetTriple::create("thumbv7em-none-eabi")).unwrap();
+    // let target_machine = target
+    //     .create_target_machine(
+    //         &TargetMachine::get_default_triple(),
+    //         "cortex-m4",
+    //         "",
+    //         OptimizationLevel::Default,
+    //         RelocMode::Default,
+    //         CodeModel::Default,
+    //     )
+    //     .unwrap();
+    //
+    // target_machine
+    //     .write_to_file(&module, FileType::Assembly, &path.with_extension("s"))
+    //     .unwrap();
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::ast::*;
+
     #[test]
     fn simple_function() {
         let source = "fn main() 0";
         let mut tokens = crate::lexer::TokenStream::new(source.into());
-        let _program =
-            crate::ast::Module::parse_body(&mut tokens, "simple_function".into()).unwrap();
+        let _program = Module::parse_body(&mut tokens, "simple_function".into()).unwrap();
     }
 
     #[test]
     fn simple_constant() {
         let source = "const x = 0";
         let mut tokens = crate::lexer::TokenStream::new(source.into());
-        let _program =
-            crate::ast::Module::parse_body(&mut tokens, "simple_constant".into()).unwrap();
+        let _program = Module::parse_body(&mut tokens, "simple_constant".into()).unwrap();
     }
 
     #[test]
     fn fn_add() {
         let source = "fn main() 1 + 2";
         let mut tokens = crate::lexer::TokenStream::new(source.into());
-        let _program = crate::ast::Module::parse_body(&mut tokens, "add".into()).unwrap();
+        let program = Module::parse_body(&mut tokens, "add".into()).unwrap();
+
+        assert_eq!(
+            program,
+            Module {
+                name: "add".into(),
+                definitions: vec![Definition::Function(Function {
+                    name: "main".into(),
+                    parameters: vec![],
+                    body: Expression::BinOp(BinOp::Add(
+                        Box::new(Expression::ConstExpression(ConstExpression::Integer(1))),
+                        Box::new(Expression::ConstExpression(ConstExpression::Integer(2))),
+                    ))
+                })],
+            },
+        )
     }
 
     #[test]
     fn fn_sub() {
         let source = "fn main() 1 - 2";
         let mut tokens = crate::lexer::TokenStream::new(source.into());
-        let _program = crate::ast::Module::parse_body(&mut tokens, "sub".into()).unwrap();
+        let program = Module::parse_body(&mut tokens, "sub".into()).unwrap();
+
+        assert_eq!(
+            program,
+            Module {
+                name: "sub".into(),
+                definitions: vec![Definition::Function(Function {
+                    name: "main".into(),
+                    parameters: vec![],
+                    body: Expression::BinOp(BinOp::Sub(
+                        Box::new(Expression::ConstExpression(ConstExpression::Integer(1))),
+                        Box::new(Expression::ConstExpression(ConstExpression::Integer(2))),
+                    ))
+                })],
+            },
+        )
     }
 
     #[test]
     fn fn_mul() {
         let source = "fn main() 1 * 2";
         let mut tokens = crate::lexer::TokenStream::new(source.into());
-        let _program = crate::ast::Module::parse_body(&mut tokens, "mul".into()).unwrap();
+        let program = Module::parse_body(&mut tokens, "mul".into()).unwrap();
+
+        assert_eq!(
+            program,
+            Module {
+                name: "mul".into(),
+                definitions: vec![Definition::Function(Function {
+                    name: "main".into(),
+                    parameters: vec![],
+                    body: Expression::BinOp(BinOp::Mul(
+                        Box::new(Expression::ConstExpression(ConstExpression::Integer(1))),
+                        Box::new(Expression::ConstExpression(ConstExpression::Integer(2))),
+                    ))
+                })],
+            },
+        )
     }
 
     #[test]
     fn fn_div() {
         let source = "fn main() 1 / 2";
         let mut tokens = crate::lexer::TokenStream::new(source.into());
-        let _program = crate::ast::Module::parse_body(&mut tokens, "div".into()).unwrap();
+        let program = Module::parse_body(&mut tokens, "div".into()).unwrap();
+
+        assert_eq!(
+            program,
+            Module {
+                name: "div".into(),
+                definitions: vec![Definition::Function(Function {
+                    name: "main".into(),
+                    parameters: vec![],
+                    body: Expression::BinOp(BinOp::Div(
+                        Box::new(Expression::ConstExpression(ConstExpression::Integer(1))),
+                        Box::new(Expression::ConstExpression(ConstExpression::Integer(2))),
+                    ))
+                })],
+            },
+        )
+    }
+
+    #[test]
+    fn fn_empty_block() {
+        let source = "fn main() {}";
+        let mut tokens = crate::lexer::TokenStream::new(source.into());
+        let program = Module::parse_body(&mut tokens, "block".into()).unwrap();
+
+        assert_eq!(
+            program,
+            Module {
+                name: "block".into(),
+                definitions: vec![Definition::Function(Function {
+                    name: "main".into(),
+                    parameters: vec![],
+                    body: Expression::Block(Block {
+                        body: vec![],
+                        last: None
+                    })
+                })],
+            },
+        )
+    }
+
+    #[test]
+    fn fn_simple_block() {
+        let source = "fn main() { 0 }";
+        let mut tokens = crate::lexer::TokenStream::new(source.into());
+        let program = Module::parse_body(&mut tokens, "simple_block".into()).unwrap();
+
+        assert_eq!(
+            program,
+            Module {
+                name: "simple_block".into(),
+                definitions: vec![Definition::Function(Function {
+                    name: "main".into(),
+                    parameters: vec![],
+                    body: Expression::Block(Block {
+                        body: vec![],
+                        last: Some(Box::new(Expression::ConstExpression(
+                            ConstExpression::Integer(0)
+                        )))
+                    })
+                })],
+            },
+        )
+    }
+
+    #[test]
+    fn fn_compound_block() {
+        let source = "fn main() { 1; 2 }";
+        let mut tokens = crate::lexer::TokenStream::new(source.into());
+        let program = Module::parse_body(&mut tokens, "compound_block".into()).unwrap();
+
+        assert_eq!(
+            program,
+            Module {
+                name: "compound_block".into(),
+                definitions: vec![Definition::Function(Function {
+                    name: "main".into(),
+                    parameters: vec![],
+                    body: Expression::Block(Block {
+                        body: vec![Expression::ConstExpression(ConstExpression::Integer(1))],
+                        last: Some(Box::new(Expression::ConstExpression(
+                            ConstExpression::Integer(2)
+                        )))
+                    })
+                })],
+            },
+        )
     }
 }
